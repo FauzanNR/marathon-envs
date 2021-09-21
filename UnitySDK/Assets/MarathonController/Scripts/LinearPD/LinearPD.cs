1497f1b7ef9d4d0334d6987ee14bd165fcd59da7
Joan Llobera <joan.llobera@artanim.ch>
2021-09-21 15:25:16 +0200

    finished first implementation of the algorithm, math net library encapsulated in PDLink class, interface with Unity in LinearPD. Pending: -Introduce the values targeted -Debug -Check how to modify it to get a PD controller

---
+++ UnitySDK/Assets/MarathonController/Scripts/LinearPD/LinearPD.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using LinearPDController;
+
+public class LinearPD : MonoBehaviour
+{
+
+
+
+
+
+   // ArticulationBody[] Links;
+
+
+    [SerializeField]
+    ArticulationBody theRoot;
+
+
+    PDLink[] PDLinks;
+
+   
+
+
+
+    void Start()
+    {
+        Init(); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ABAalgo();
+
+    }
+
+
+    void Init()
+    {
+        if (theRoot == null | !theRoot.isRoot)
+        {
+            Debug.LogError("LinearPD does not know who is the root of the articulationBody Hierarchy");
+        }
+
+
+
+        PDLinks = PDLink.SortLinks(theRoot);
+
+
+    }
+
+
+
+
+    void ABAalgo() {
+
+        //PASS 1 in Featherstone
+        ComputeTreeLinkVelocities();
+
+       
+        InitTreeLinks();
+
+        //PASS 2 and 3 in Featherstone
+        TreeFwdDynamics();
+
+
+
+    }
+
+
+
+
+    //Mirtich pge 116
+    void ComputeTreeLinkVelocities() {
+
+        //TODO:
+        //input is qVel for each articulated body
+        float qVel = 0;
+
+
+
+        //foreach (ArticulationBody ab in Links) {
+        foreach (PDLink pdl in PDLinks)
+        {
+            pdl.updateVelocities(qVel);
+            
+        }
+    
+    
+    }
+
+
+    //Mirtich page 120
+    void InitTreeLinks() 
+    {
+
+        
+
+
+        for (int i = 0; i < PDLinks.Length; i++)
+        {
+            //TODO: input is qVel for each articulated body. Introduce this value.
+            float qVel = 0;
+
+            //we put the isolated zero acceleration force  Z_i (Mirtich) or Bias Force p_i^A (Featherstone14)
+
+            //A. we calculate Z_i
+            PDLinks[i].calculateZ_i();
+
+
+            //B. We calculate I_i
+            PDLinks[i].calculateI_i();
+
+            //C. We calculate c_i
+            PDLinks[i].updateCoriolis(qVel);
+
+
+        }
+
+
+    }
+
+
+
+
+   
+
+    //Mirtich page 121
+    void TreeFwdDynamics()
+    {
+        //pass 2
+        for (int i = PDLinks.Length; i > 1; i--)
+        {
+            //TODO: update and find the target forces to apply            
+            Vector3 Qforces = Vector3.zero;
+            Vector3 Qtorques = Vector3.zero;
+
+            PDLinks[i].updateIZ_dad(Qforces,Qtorques);
+
+
+        }
+
+        //pass 3
+        for (int i = 0; i < PDLinks.Length; i++) 
+        {
+
+            Vector3 Qforces = Vector3.zero;
+            Vector3 Qtorques = Vector3.zero;
+            PDLinks[i].updateAcceleration(Qforces, Qtorques);
+        
+        
+        }
+
+    }
+
+
+
+}
---
+++ UnitySDK/Assets/MarathonController/Scripts/LinearPD/PDLink.cs
@@ -0,0 +1,391 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+//using MathNet.Numerics.LinearAlgebra.Double;
+
+
+namespace LinearPDController { 
+
+public class PDLink
+{
+
+
+
+    ArticulationBody _ab;
+
+    //equation is:
+    //f_I = I_i a_i + p_i
+
+    //CORIOLIS, GRAVITY AND OTHER EXTERNAL FORCES
+    Vector<double> Z_i; // Vector<double>.Build.Dense(6);
+                            // we use this variable to store 2 values:
+                            //  1. isolated zero acceleration force  Z_i (Mirtich) or Bias Force p_i^A (Featherstone14)
+                            //  2. spatial articulated zero-acceleration force  Z_i^A (Mirtich) or articulated Bias Force p_i^a (Featherstone14)
+                            //When put together in a big matrix, we write C(q, q^dot) 
+
+    Vector<double> c_i;  //Vector<double>.Build.Dense(6);
+                            //Spatial Coriolis Force
+
+    //INERTIA or generalized Mass
+    Matrix<double> I_i;  //Matrix<double>.Build.Dense(6, 6);
+                                //we use this variable to store 2 values:
+                                //  1. spatial isolated inertia I_i (Mirtich) or rigid-body inertia I_i (Featherstone14)
+                                //  2. spatial articulated inertia I_i^A (Mirtich) or rigid-body apparent  inertia I_i^a  (Featherstone14)
+                                //When put together in a big matrix, we often write M(q), sometimes also H
+
+
+    Vector<double> acceleration;
+
+    PDLink dad;
+
+    //Inertia inverse, kept here just to not calculate it twice (in passes 2 and 3):
+    Matrix<double> SISInverted;
+
+    PDLink()
+    {
+    }
+
+
+    PDLink(ArticulationBody a, List<PDLink> SortedPDLinks)
+    {
+
+        PDLink me = SortedPDLinks.FirstOrDefault(x => x._ab.Equals(a));
+        if (me == null)
+        {
+            PDLink pdl = new PDLink();
+            pdl._ab = a;
+
+
+            PDLink dad = null;
+            ArticulationBody abdad = a.transform.parent.GetComponent<ArticulationBody>();
+            if (abdad != null)
+            {
+                dad = SortedPDLinks.FirstOrDefault(x => x._ab.Equals(abdad));
+
+            }
+            else
+            {
+                Debug.Log(a.name + " does not seem to have an articulationBody as parent");
+
+            }
+
+            if (dad != null)
+            {
+                pdl.dad = dad;
+
+            }
+            else
+            {
+                Debug.LogError(a.name + "'s dad does not seem to be in the list of sorted links, this should not happen in a sorted list");
+
+
+            }
+
+            pdl.acceleration = Vector<double>.Build.Dense(6);
+
+
+        }
+
+    }
+
+    public ArticulationBody ArticulationBody { get => _ab; }
+
+    public static PDLink[] SortLinks(ArticulationBody root)
+    {
+
+        ArticulationBody[] ts = root.GetComponentsInChildren<ArticulationBody>();
+
+
+
+        PDLink[] sortedLinks = new PDLink[ts.Length];
+
+
+
+
+        NumberLinks(root, 1);
+
+
+        return sortedLinks;
+
+
+        int NumberLinks(ArticulationBody node, int idx)
+        {
+
+            PDLink PDnode = new PDLink(node, sortedLinks.ToList<PDLink>());
+
+            sortedLinks[idx - 1] = PDnode;
+            idx += 1;
+            //notice this is recursive
+            foreach (Transform child in node.transform)
+            {
+                ArticulationBody abchild = child.GetComponent<ArticulationBody>();
+                if (abchild != null)
+                    idx = NumberLinks(abchild, idx);
+            }
+
+            return idx;
+
+
+        }
+
+    }
+
+    #region pass1
+
+    //Mirtich pge 116
+    public void updateVelocities(float qVel) {
+        ArticulationBody ab = this.ArticulationBody;
+        Quaternion Rot = ab.transform.localRotation;
+
+        ArticulationBody dad = ab.transform.parent.GetComponent<ArticulationBody>();
+        Vector3 Rad = ab.centerOfMass - dad.centerOfMass;
+
+
+        //w_i = Rot * w_h
+        ab.angularVelocity = Rot * dad.angularVelocity;
+
+        //v_i = Rot * w_i x r
+        ab.velocity = Rot * dad.velocity + Vector3.Cross(ab.angularVelocity, Rad);
+
+
+        Vector3 d_i = get_d(ab);
+        Vector3 u_i = get_u(ab);
+
+        ab.angularVelocity += qVel * u_i;
+        ab.velocity += qVel * Vector3.Cross(u_i, d_i);
+
+    }
+
+    public Vector<double> calculateZ_i() { 
+    //A. we calculate Z_i
+        Vector3 tmp = _ab.inertiaTensor;
+        tmp.Scale(_ab.angularVelocity);
+        tmp = Vector3.Cross(_ab.angularVelocity, tmp);
+           
+                //TODO: sort out if here we can put more than gravity
+                double[] a = { 0.0, -1.0 * _ab.mass * 9.81, 0.0, tmp[0], tmp[1], tmp[2] };
+
+        Z_i = Vector<double>.Build.Dense(a);
+        return Z_i;
+    }
+
+    //B. We calculate I_i
+    public Matrix<double> calculateI_i() { 
+        Matrix<double> I_i = Matrix<double>.Build.Dense(6, 6);
+        
+        //this could be done only once
+        I_i[0, 3] = _ab.mass;
+        I_i[1, 4] = _ab.mass;
+        I_i[2, 5] = _ab.mass;
+
+        //this has to be done in each frame:
+        I_i[4, 0] = _ab.inertiaTensor.x;
+        I_i[5, 1] = _ab.inertiaTensor.y;
+        I_i[6, 2] = _ab.inertiaTensor.z;
+
+        this.I_i = I_i;
+
+        return this.I_i;
+
+    }
+
+    //C. We calculate c_i
+    public Vector<double> updateCoriolis(float qVel) {
+
+        ArticulationBody ab = this.ArticulationBody;
+        Vector3 vel_i = qVel * get_u(ab);
+        ArticulationBody dad = ab.transform.parent.GetComponent<ArticulationBody>();
+
+        Vector3 d_i = get_d(ab);
+        //Vector3 r_i = get_r(ab, dad);
+
+        Vector3 tmp = Vector3.Cross(dad.angularVelocity, vel_i);
+        Vector3 tmp2 = Vector3.Cross(dad.angularVelocity, vel_i)
+                        + 2 * Vector3.Cross(dad.angularVelocity, Vector3.Cross(vel_i, d_i))
+                        + Vector3.Cross(vel_i, Vector3.Cross(vel_i, d_i));
+
+        double[] c = { tmp[0], tmp[1], tmp[2], tmp2[0], tmp2[1], tmp[2] };
+        this.c_i = Vector<double>.Build.Dense(c);
+        return this.c_i;
+    }
+
+
+
+    #endregion
+
+
+
+
+    #region pass2
+
+    //treeFwdDynamics Mirtich page 121
+    public void updateIZ_dad(Vector3 forces, Vector3 torques)
+    {
+
+         //A. we calculate I_h^A
+        ArticulationBody ab = this._ab;
+        ArticulationBody abdad = this.dad.ArticulationBody;
+
+        Vector3 u = get_u(ab);
+        Vector3 uxd = Vector3.Cross(u, get_d(ab));
+        double[] a = { u[0], u[1], u[2], uxd[0], uxd[1], uxd[2] };
+        Vector<double> S_i = Vector<double>.Build.Dense(a);
+        Matrix<double> S_iT = spatialTranspose(S_i);//it is a row matrix
+        Matrix<double> dadXab = get_bXa(ab, abdad);
+        Matrix<double> abXdad = get_bXa(abdad, ab);
+
+
+        //update I_dad
+        Matrix<double> tmp = I_i * S_i.ToColumnMatrix() * S_iT * I_i;
+         SISInverted = (S_iT * I_i * S_i.ToColumnMatrix()).Inverse();
+        dad.I_i += dadXab * (I_i - SISInverted * tmp) * abXdad;
+
+
+
+        //B. we calculate Z_h^A
+       //update Z_dad
+        double[] q = { forces.x, forces.y, forces.z, torques.x, torques.y, torques.z };
+        Vector<double> Q = Vector<double>.Build.Dense(q);
+
+        Vector<double> tmp3 = I_i * S_i.ToColumnMatrix() * (Q - S_iT * (Z_i + I_i * c_i));
+        dad.Z_i += dadXab * (Z_i + I_i * c_i + (SISInverted * tmp3));
+
+
+    }
+
+        #endregion
+
+    #region pass3
+
+        public Vector<double> updateAcceleration(Vector3 forces, Vector3 torques) 
+        {
+
+            double[] q = { forces.x, forces.y, forces.z, torques.x, torques.y, torques.z };
+            Vector<double> Q = Vector<double>.Build.Dense(q);
+
+
+            Vector3 u = get_u(_ab);
+            Vector3 uxd = Vector3.Cross(u, get_d(_ab));
+            double[] a = { u[0], u[1], u[2], uxd[0], uxd[1], uxd[2] };
+            Vector<double> S_i = Vector<double>.Build.Dense(a);
+            Matrix<double> S_iT = spatialTranspose(S_i);//it is a row matrix
+                                                        //Matrix<double> dadXab = get_bXa(_ab, abdad);
+
+            ArticulationBody abdad = this.dad.ArticulationBody;
+            Matrix<double> abXdad = get_bXa(abdad, this._ab);
+
+            //TODO: check how we deal with a(0) = 0;
+
+            Vector<double> qAccel = SISInverted * (Q - S_iT * abXdad * dad.acceleration - S_iT * (Z_i + I_i * c_i));
+
+            acceleration = abXdad * dad.acceleration + c_i + qAccel * S_i;
+
+            return qAccel;
+        }
+
+
+    #endregion
+
+
+        #region utils
+        static Matrix<double> spatialTranspose(Vector<double> input)
+    {
+        double[] oarray = { input[3], input[4], input[5], input[0], input[1], input[2] };
+        Vector<double> output = Vector<double>.Build.Dense(oarray);
+        return output.ToRowMatrix();
+    }
+
+    static Vector3 get_u(ArticulationBody ab)
+    {
+        Vector3 u_i = new Vector3();
+        ab.transform.rotation.ToAngleAxis(out _, out u_i);
+        return u_i;
+
+    }
+
+
+    static Vector3 get_d(ArticulationBody ab)
+    {
+        return (ab.centerOfMass - ab.anchorPosition);
+
+    }
+
+
+
+
+
+    static Vector3 get_r(ArticulationBody ab, ArticulationBody dad)
+    {
+        return (ab.centerOfMass - dad.centerOfMass);
+
+
+    }
+
+
+
+    //see Mirtich appendix A.2, page 230
+    static Matrix<double> get_rtilde(Vector3 r)
+    {
+
+
+        //we are describing column by column:
+        double[] rtarray = {    0  , r.z,-r.y ,
+                               -r.z, 0  , r.x ,
+                                r.y,-r.x, 0    };
+
+        Matrix<double> rt = Matrix<double>.Build.Dense(3, 3, rtarray);
+        return rt;
+    }
+
+    //return a 3x3 matrix rotation from a quaternion
+    //see https://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation
+    static Matrix<double> get_Rot3x3(Quaternion q)
+    {
+
+        // in this list each row represents a column in the resulting matrix:
+        // we list describing column by column, assuming the norm of the quaternion is 1
+        double[] Rarray = { 1 - 2 * (q.y * q.y + q.z * q.z),  2* (q.x * q.y + q.z * q.w )   , 2* (q.x * q.z - q.y * q.w)      ,
+                            2* (q.x * q.y - q.z * q.w )    , 1 - 2 * (q.x * q.x + q.z * q.z), 2* (q.y * q.z + q.x * q.w )     ,
+                            2* (q.x * q.z + q.y * q.w)     , 2* (q.y * q.z - q.x * q.w )    , 1 - 2 * (q.x * q.x + q.y * q.y) };
+
+        Matrix<double> R = Matrix<double>.Build.Dense(3, 3, Rarray);
+        return R;
+
+    }
+
+    //Returns the spatial transform matrix that goes from frame of reference a to frame of reference b
+    static Matrix<double> get_bXa(ArticulationBody a, ArticulationBody b)
+    {
+
+
+        //the spatial transformation matrix, see Mirtich page 102
+
+        Matrix<double> rtild = get_rtilde(get_r(a, b));
+
+        //Matrix<double> R = get_R(ab.transform.localRotation);
+        Quaternion localRot = a.transform.rotation * Quaternion.Inverse(b.transform.rotation);//todo that when a is son of b, it is equivalent to a.transform.localRotation;
+
+        Matrix<double> R = get_Rot3x3(localRot);
+
+
+        //this builds it by rows?
+        Matrix<double>[,] x = { { R, Matrix<double>.Build.Dense(3, 3)},
+                                    { -rtild * R, R                      } };
+
+        Matrix<double> bXa = Matrix<double>.Build.DenseOfMatrixArray(x);
+        return bXa;
+
+    }
+
+    #endregion
+
+}
+
+
+
+}//namespace
+
+
