bb0115853bfb5db4812adc82eb1322ddac418795
Joan Llobera <joan.llobera@artanim.ch>
2022-04-27 08:30:42 +0200

    moving around some scripts to make assemblies find each other

--- UnitySDK/Assets/MarathonEnvs/Scripts/Ragdoll004/RangeOfMotionValues.cs
+++ UnitySDK/Assets/MarathonEnvs/Scripts/Ragdoll004/RangeOfMotionValues.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using Unity.Barracuda;
+//using Unity.Barracuda;
 
 [System.Serializable]
 public class RangeOfMotionValue
