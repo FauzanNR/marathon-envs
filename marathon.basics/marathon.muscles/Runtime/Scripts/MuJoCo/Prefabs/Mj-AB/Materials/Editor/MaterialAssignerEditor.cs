e1b9c559ee11dff4e3e2cfbe9e5bbbf802a53de3
Joan Llobera <joan.llobera@artanim.ch>
2022-08-04 11:42:52 +0200

    some small tweaks so it can laos run when developing in an external example

--- marathon.basics/marathon.muscles/Runtime/Scripts/MuJoCo/Prefabs/Mj-AB/Materials/Editor/MaterialAssignerEditor.cs
+++ marathon.basics/marathon.muscles/Runtime/Scripts/MuJoCo/Prefabs/Mj-AB/Materials/Editor/MaterialAssignerEditor.cs
@@ -1,3 +1,5 @@
+#if UNITY_EDITOR
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,3 +26,5 @@ public class MaterialAssignerEditor : Editor
 
     }
 }
+
+#endif
